041d0a7f8a40b1cfdf3ab14205e354f09be6abbb
tharusha dilhara jayadeera <104320538+tharusha-dilhara@users.noreply.github.com>
2023-02-22 02:49:59 +0530

    edit
    edit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-    res.send('web server is working and edit is successfull');
+    res.send('web server is working and edit is successfull ff');
 })
 
 app.listen(port,()=>{
